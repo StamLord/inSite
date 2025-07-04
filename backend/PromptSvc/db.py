f2874a85c786b7ecaab4f525663e5363dbd01965
StamLord <42681607+StamLord@users.noreply.github.com>
2025-07-04 15:30:02 +0300

    Cleaned DB env vars

--- backend/PromptSvc/db.py
+++ backend/PromptSvc/db.py
@@ -5,13 +5,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
-username = os.getenv("DATABASE_USER")
-password = os.getenv("DATABASE_PASS")
-host = os.getenv("DATABASE_HOST")
-port = os.getenv("DATABASE_PORT")
-dbname = os.getenv("DATABASE_NAME")
-
-DATABASE_URL = f"postgresql://postgres:{password}@db.{username}.supabase.co:{port}/postgres"
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
