84b142d24c79bac255f2b905bca1b59aea0ab29a
StamLord <42681607+StamLord@users.noreply.github.com>
2025-08-29 01:05:04 +0300

    Logo added

---
+++ frontend/src/components/InsiteLogo.jsx
@@ -0,0 +1,6 @@
+import { ReactComponent as Logo } from "../assets/inSITE_text.svg";
+import styles from "./InsiteLogo.module.css";
+
+export default function InsiteLogo() {
+    return <Logo className={styles.logo}/>
+}
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './Navbar.module.css';
 import { Link } from 'react-router-dom';
+import InsiteLogo from './InsiteLogo';
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
         <div className={styles.nav_logo}>
-            <Link to="/">INSITE</Link>
+            <Link to="/"><InsiteLogo/></Link>
         </div>
         <div className={styles.nav_center}>
             <a href="#">Platform</a>
