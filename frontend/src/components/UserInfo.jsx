dab6d5d960a00d4dbba90341abf98335c3606939
StamLord <42681607+StamLord@users.noreply.github.com>
2025-09-18 12:11:32 +0300

    Replaced login with suggestion button

--- frontend/src/components/UserInfo.jsx
+++ frontend/src/components/UserInfo.jsx
@@ -5,20 +5,28 @@ import { useAuth } from "../context/AuthContext";
 export default function UserInfo() {
     const navigate = useNavigate();
     const { user } = useAuth();
+    const disableUser = true;
 
     return (
         <div className={styles.loginContainer}>
-            {user? 
-                <div className={styles.welcomeWrapper}>
-                    <span className={styles.welcomeMessage}>Logged in as {user.email.split("@")[0]}</span>
-                    <button className={styles.loginButton}>Logout</button>
-                </div>
-            :
-                <>
-                    <button className={styles.loginButton} onClick={() => {navigate('/account')}}>Login</button>
-                    <button className={styles.demoButton}>Get a Demo</button>
-                </>
-             }
+            {disableUser? 
+                    <a 
+                        className={styles.loginButton}
+                        href="https://docs.google.com/forms/d/e/1FAIpQLSfxWxiMF8ZM_Ymg0jF2H-HsMwJk525zDgf2v2t6ght80asBog/viewform">
+                        💡 Leave a suggestion
+                    </a>
+                :
+                user? 
+                    <div className={styles.welcomeWrapper}>
+                        <span className={styles.welcomeMessage}>Logged in as {user.email.split("@")[0]}</span>
+                        <button className={styles.loginButton}>Logout</button>
+                    </div>
+                :
+                    <>
+                        <button className={styles.loginButton} onClick={() => {navigate('/account')}}>Login</button>
+                        <button className={styles.demoButton}>Get a Demo</button>
+                    </>
+            }
         </div>
     );
 }
